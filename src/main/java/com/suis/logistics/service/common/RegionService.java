df9c532c1460edc1c741da9055685da15fd9c8a6
Iswar <iswar.java9@gmail.com>
2017-09-30 21:05:14 +0530

    @Transactional added in Service Level

--- src/main/java/com/suis/logistics/service/common/RegionService.java
+++ src/main/java/com/suis/logistics/service/common/RegionService.java
@@ -2,10 +2,13 @@ package com.suis.logistics.service.common;
 
 import java.util.List;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import com.suis.logistics.model.City;
 import com.suis.logistics.model.TimeZone;
 
 
+@Transactional
 public interface RegionService {
 	public List<City> getCitiesByName(String name);
 	public List<TimeZone> getAvailableTimeZonesByCountry(String countryCode);
